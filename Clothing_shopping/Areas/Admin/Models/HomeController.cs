2987df4d0d999b91ec644175ab2320b06ef8c7a3
ThinhIT137 <tranthinh130720052@gmail.com>
2025-11-21 21:16:13 +0700

    Thêm admin - quản lý sản phẩm, quản lý đơn hàng

---
+++ Clothing_shopping/Areas/Admin/Models/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clothing_shopping.Areas.Admin.Models
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
